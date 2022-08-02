dd60df20cc45a5d2764ebcb77ab371501940123a
Nakseung Choi <chlskrtmd123@gmail.com>
2022-08-02 00:47:47 -0700

    initial commit

---
+++ 1_blank/ses/Output/Debug/Obj/blinky_pca10056/app_util_platform.d
@@ -0,0 +1,34 @@
+Output/Debug/Obj/blinky_pca10056/app_util_platform.o: \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\components\libraries\util\app_util_platform.c \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\components\libraries\util\app_util_platform.h \
+ C:/Users/chlsk/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdint.h \
+ C:/Users/chlsk/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/__config \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/__SEGGER_RTL.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/__SEGGER_RTL_ConfDefaults.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/__SEGGER_RTL_Conf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/__SEGGER_RTL_Arm_Conf.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\components\libraries\util\nrf_assert.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\components\libraries\util\app_error.h \
+ C:/Users/chlsk/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdio.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/stdio.h \
+ C:/Users/chlsk/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdbool.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/stdbool.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\components\libraries\util\sdk_errors.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\components\libraries\util\nordic_common.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\components\libraries\util\app_error_weak.h
---
+++ 1_blank/ses/Output/Debug/Obj/blinky_pca10056/system_nrf52840.d
@@ -0,0 +1,24 @@
+Output/Debug/Obj/blinky_pca10056/system_nrf52840.o: \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\modules\nrfx\mdk\system_nrf52840.c \
+ C:/Users/chlsk/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdint.h \
+ C:/Users/chlsk/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/__config \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/__SEGGER_RTL.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/__SEGGER_RTL_ConfDefaults.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/__SEGGER_RTL_Conf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/__SEGGER_RTL_Arm_Conf.h \
+ C:/Users/chlsk/AppData/Local/SEGGER/SEGGER\ Embedded\ Studio/v3/packages/libraries/libcxx/include/stdbool.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 6.30/include/stdbool.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\modules\nrfx\mdk\nrf.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\modules\nrfx\mdk\nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\modules\nrfx\mdk\system_nrf52840.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\modules\nrfx\mdk\system_nrf.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\modules\nrfx\mdk\nrf52840_bitfields.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\modules\nrfx\mdk\nrf51_to_nrf52840.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\modules\nrfx\mdk\nrf52_to_nrf52840.h \
+ C:\nrf_sdk\DeviceDownload\nRF5SDK160098a08e2\modules\nrfx\mdk\compiler_abstraction.h
